871c2f459dbe174d044bc850b5d9c2f069c9b090
luis barrera <luisbarrerazelaya3@gmail.com>
2024-02-01 17:44:43 -0600

    login

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+var usuario1 = "YoSoyLuis"
+
+var contraseña1 = "12345678"
+
+function sesion(){
+    var usuario = (document.getElementById("usuario")).value;
+    var contraseña = (document.getElementById("contraseña")).value;
+    if(usuario1 == usuario && contraseña1 == contraseña){
+        alert("sesion iniciada");
+        window.location.href = "main.html";
+    }
+    else{
+        alert("Error en las credenciales");
+    }
+}
+
+
+function cerrarSesion(){
+    window.location.href = "login.html";
+}
+
+
