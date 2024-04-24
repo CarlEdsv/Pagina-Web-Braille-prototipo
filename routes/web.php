a4c9dbed133596a054e7fd8b9652735c0844e2cd
Luis Barrera <109974057+YoSoyLuis503@users.noreply.github.com>
2024-04-23 18:31:01 -0600

    nicki nicole, si o no?

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('login', function () {
     return view('login');
 });
 
-Route::get('main', function () {
+Route::get('/', function () {
     return view('main');
 });
